835bd735f083d369d89cd001c73567773c95e014
SolidityDevs <101315257+SolidityDevs@users.noreply.github.com>
2022-03-18 10:53:25 +0100

    Add files via upload

---
+++ apis/cmc.py
@@ -0,0 +1,66 @@
+import aiohttp
+import pandas as pd
+from coinmarketcapapi import CoinMarketCapAPI
+
+from app import logger
+from config import COIN_MARKET_CAP_API_KEY, HEADERS
+
+
+class CoinMarketCap:
+    def __init__(self):
+        self.cmc = CoinMarketCapAPI(COIN_MARKET_CAP_API_KEY)
+
+    def get_coin_ids(self, symbol: str) -> list:
+        """
+        Retrieves coin ids for matching symbol
+        Args:
+            symbol (str): Token symbol
+        Returns (list): List of token ids
+        """
+        logger.info("Looking up token ids for %s in CoinMarketCap API", symbol)
+        return [
+            (str(item["id"]), item["name"])
+            for item in self.cmc.cryptocurrency_map(symbol=symbol).data
+        ]
+
+    def get_coin_metadata(self, ids: str) -> dict:
+        """
+        Retrieves coin metadata
+        Args:
+            ids (str): Token id
+        Returns (list): Metadata for provided coin ids
+        """
+        return self.cmc.cryptocurrency_info(id=ids).data
+
+    def coin_lookup(self, ids: str) -> dict:
+        """Coin lookup in CoinMarketCap API
+        Args:
+            ids (str): CoinMarketCap token ids
+        Returns:
+            dict: Results of coin lookup
+        """
+        logger.info("Looking up price for %s in CoinMarketCap API", ids)
+        return self.cmc.cryptocurrency_quotes_latest(id=ids, convert="usd").data
+
+    @staticmethod
+    async def get_trending_coins() -> list:
+        """
+        Scalps trending coins from CoinMarketCap website
+        Returns (list): Trending coins
+        """
+        logger.info("Retrieving trending coins from CoinMarketCap")
+        coins = []
+        async with aiohttp.ClientSession() as session, session.get(
+            "https://coinmarketcap.com/trending-cryptocurrencies/", headers=HEADERS
+        ) as response:
+            df = pd.read_html(await response.text(), flavor="bs4")[0]
+
+            for row in df.itertuples():
+                if row.Index > 6:
+                    break
+                name = row.Name.replace(f"{row.Index + 1}", " ")
+                words = name.split()
+                words[-1] = f"({words[-1]})"
+                coin = " ".join(words)
+                coins.append(coin)
+        return coins
---
+++ apis/coingecko.py
@@ -0,0 +1,96 @@
+from aiocoingecko import AsyncCoinGeckoAPISession
+from requests.exceptions import RequestException
+
+from app import logger
+from lru import LRU
+#from coin import coingecko_coin_lookup_cache
+coingecko_coin_lookup_cache = LRU(5)
+
+class CoinGecko:
+    def __init__(self):
+        self.cg = AsyncCoinGeckoAPISession()
+
+    async def coin_lookup(self, ids: str, is_address: bool = False) -> dict:
+        logger.info("Looking up price for %s in CoinGecko API", ids)
+        async with self.cg as cg:
+            try:
+                data = (
+                    await cg.get_coin_info_from_contract_address_by_id(
+                        platform_id="ethereum", contract_address=ids
+                    )
+                    if is_address
+                    else await cg.get_coin_by_id(coin_id=ids)
+                )
+            except ValueError:
+                data = await cg.get_coin_info_from_contract_address_by_id(
+                    platform_id="binance-smart-chain", contract_address=ids
+                )
+            except RequestException:
+                data = (
+                    await cg.get_coin_info_from_contract_address_by_id(
+                        platfomr_id="binance", contract_address=ids
+                    )
+                    if is_address
+                    else await cg.get_coin_by_id(coin_id=ids)
+                )
+        return data
+
+    async def get_trending_coins(self) -> list:
+        """
+        Gets trending coins
+        Returns (list): Trending coins
+
+        """
+        logger.info("Retrieving CoinGecko trending coins")
+
+        async with self.cg as cg:
+            trending_coins = await cg.get_search_trending()
+        return trending_coins["coins"]
+
+    async def coin_market_lookup(
+        self, ids: str, time_frame: int, base_coin: str
+    ) -> dict:
+        """Coin lookup in CoinGecko API for Market Chart
+
+        Args:
+            ids (str): id of coin to lookup
+            time_frame (int): Indicates number of days for data span
+            base_coin (str): Indicates base coin
+
+        Returns:
+            dict: Data from CoinGecko API
+        """
+        logger.info("Looking up chart data for %s in CoinGecko API", ids)
+
+        async with self.cg as cg:
+            return await cg.get_coin_market_chart_by_id(ids, base_coin, time_frame)
+
+    async def get_coin_ids(self, symbol: str) -> list:
+        """Retrieves coin stats from connected services crypto services
+
+        Args:
+            symbol (str): Cryptocurrency symbol of coin to lookup
+
+        Returns:
+            list: coin ids of matching search results for given symbol
+        """
+        logger.info("Getting coin ID for %s", symbol)
+        coin_ids = []
+        if symbol in coingecko_coin_lookup_cache.keys():
+            coin_ids.append(coingecko_coin_lookup_cache[symbol])
+        else:
+            async with self.cg as cg:
+                coins = [
+                    coin
+                    for coin in await cg.get_coins_list()
+                    if coin["symbol"].upper() == symbol
+                ]
+
+            for coin in coins:
+                coin_id = coin["id"]
+                coin_ids.append(coin_id)
+
+            if len(coin_ids) == 1:
+                coingecko_coin_lookup_cache[symbol] = coin_ids[0]
+
+        return coin_ids
